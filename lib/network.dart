1b9c3d5e02d6008243e1cf4337ed17ac1c07f996
sunin457 <sunin457@gmail.com>
2022-10-28 16:45:30 +0700

    first

---
+++ lib/network.dart
@@ -0,0 +1,8 @@
+library network;
+
+// model
+export 'model/movie.dart';
+export 'model/genres.dart';
+
+// network
+export 'network/api_provider.dart';
---
+++ lib/network/api_provider.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart' show Client;
+import 'package:network/model/movie.dart';
+
+class ApiProvider {
+  Client client = Client();
+
+  //TODO: Add your api key here
+  static final _apiKey = 'a6ef39240a083e148595040839eb9408';
+  static final String _baseUrl = 'http://api.themoviedb.org/3/movie';
+
+  Future<Movie> getMovieList() async {
+    final url = Uri.parse("$_baseUrl/popular?api_key=$_apiKey");
+    final response = await client.get(url);
+    if (response.statusCode == 200) {
+      return Movie.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load movie');
+    }
+  }
+}
